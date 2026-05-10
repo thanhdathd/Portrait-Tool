69d330473f2c71f41ded8e69ef71754c746e5989
Thanh Dat <phithanhdathd@gmail.com>
2026-05-10 20:34:16 +0700

    fix resize command with point and grid

--- src/main/java/core/history/ResizeCommand.java
+++ src/main/java/core/history/ResizeCommand.java
@@ -13,54 +13,64 @@ public class ResizeCommand implements Command {
     private final CanvasState canvasState;
     private final BufferedImage oldImage;
     private final BufferedImage newImage;
-    private final List<SPoint> oldPoints;
-    private final List<SPoint> oldGrids;
+    private final List<PointSnapshot> undoPoints = new ArrayList<>();
+    private final List<PointSnapshot> undoGrids = new ArrayList<>();
     private final double scaleX;
     private final double scaleY;
+
+    private static class PointSnapshot {
+        final SPoint point;
+        final int oldX;
+        final int oldY;
+
+        PointSnapshot(SPoint point) {
+            this.point = point;
+            this.oldX = point.X;
+            this.oldY = point.Y;
+        }
+        
+        void restore() {
+            point.X = oldX;
+            point.Y = oldY;
+        }
+    }
 
     public ResizeCommand(ImageCanvas canvas, CanvasState canvasState, BufferedImage oldImage, BufferedImage newImage) {
         this.canvas = canvas;
         this.canvasState = canvasState;
         this.oldImage = oldImage;
         this.newImage = newImage;
-        this.oldPoints = clonePoints(canvasState.getStickyPoints());
-        this.oldGrids = clonePoints(canvasState.getGrids());
         this.scaleX = (double) newImage.getWidth() / oldImage.getWidth();
         this.scaleY = (double) newImage.getHeight() / oldImage.getHeight();
-    }
-
-    private List<SPoint> clonePoints(List<SPoint> points) {
-        List<SPoint> cloned = new ArrayList<>();
-        for (SPoint p : points) {
-            SPoint newP = new SPoint(p.id, p.X, p.Y, p.c, p.dr);
-            newP.isCustomPlacement = p.isCustomPlacement;
-            newP.customGap = p.customGap;
-            newP.customAngle = p.customAngle;
-            cloned.add(newP);
-        }
-        return cloned;
     }
 
     @Override
     public void execute() {
         canvas.setBackgroundImage(newImage);
-        scalePoints(canvasState.getStickyPoints());
-        scalePoints(canvasState.getGrids());
+        
+        undoPoints.clear();
+        for (SPoint p : canvasState.getStickyPoints()) {
+            undoPoints.add(new PointSnapshot(p));
+            p.X = (int) Math.round(p.X * scaleX);
+            p.Y = (int) Math.round(p.Y * scaleY);
+        }
+
+        undoGrids.clear();
+        for (SPoint p : canvasState.getGrids()) {
+            undoGrids.add(new PointSnapshot(p));
+            p.X = (int) Math.round(p.X * scaleX);
+            p.Y = (int) Math.round(p.Y * scaleY);
+        }
     }
 
     @Override
     public void undo() {
         canvas.setBackgroundImage(oldImage);
-        canvasState.getStickyPoints().clear();
-        canvasState.getStickyPoints().addAll(oldPoints);
-        canvasState.getGrids().clear();
-        canvasState.getGrids().addAll(oldGrids);
-    }
-    
-    private void scalePoints(List<SPoint> points) {
-        for (SPoint p : points) {
-            p.X = (int) Math.round(p.X * scaleX);
-            p.Y = (int) Math.round(p.Y * scaleY);
+        for (PointSnapshot ps : undoPoints) {
+            ps.restore();
+        }
+        for (PointSnapshot ps : undoGrids) {
+            ps.restore();
         }
     }
 }
